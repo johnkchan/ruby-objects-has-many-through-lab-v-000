6b22fa9c16472bc46a3cc85ba510ac97df452854
Jonathan Chan <johnkchan252@gmail.com>
2018-08-19 00:33:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,4 +14,14 @@ class Song
     @@all << self
   end
   
+    
+  def songs()
+    @@all.each do |song|
+      if song.artist == @name
+        @songs << song
+      end
+    end
+    @songs
+  end
+  
 end
