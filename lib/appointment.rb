bae1faaa1deaedbb6cfe95ea785908029b323cd7
Jonathan Chan <johnkchan252@gmail.com>
2018-08-19 01:04:31 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,10 +7,12 @@ class Appointment
     @@all
   end
   
-  def initialize(date, patient, doctor)
-    @date = date
+  # hevydevy, doctor_who, "Friday, January 32nd"
+  
+  def initialize(patient, doctor, date)
     @patient = patient
     @doctor = doctor
+    @date = date
     @@all << self
   end
   
